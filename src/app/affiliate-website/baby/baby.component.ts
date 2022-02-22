d2123418ef85fc9fb2b2c2120abbb892853f8e14
Tinkusingh0803 <singhtinku0803@gmail.com>
2022-02-22 22:33:54 +0530

    add new feature in baby component

--- src/app/affiliate-website/baby/baby.component.ts
+++ src/app/affiliate-website/baby/baby.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-baby',
   templateUrl: './baby.component.html',
@@ -9,7 +8,37 @@ export class BabyComponent implements OnInit {
 
   constructor() { }
 
+data= [
+  {
+     "image": "https://images-na.ssl-images-amazon.com/images/G/31/img19/Baby/FNovember/BabyStore/NEW/D32463504_Slider_tile_dipers_set_final_01._CB650685915_.jpg",
+     "discription": "Diapers & wipe"
+
+  },
+  {
+    "image": "https://images-na.ssl-images-amazon.com/images/G/31/img19/Baby/FNovember/BabyStore/Bathskincar/D32463504_Slider_tile_dipers_set_final_01_05_Beybee._CB628325514_.jpg",
+    "discription": "Bath & skin Care"
+
+ },
+ {
+  "image": "https://images-na.ssl-images-amazon.com/images/G/31/img19/Baby/FNovember/BabyStore/NEW/D32463504_Slider_tile_dipers_set_final_04._CB650685915_.jpg",
+  "discription": "Baby travel"
+
+},
+{
+  "image": "https://images-na.ssl-images-amazon.com/images/G/31/img19/Baby/FNovember/BabyStore/NEW/D32463504_Slider_tile_dipers_set_final_02._CB650685915_.jpg",
+  "discription": "Bedding & nursry"
+
+}, {
+  "image": "https://images-na.ssl-images-amazon.com/images/G/31/img19/Baby/FNovember/BabyStore/NEW/D32463504_Slider_tile_dipers_set_final_03._CB650685912_.jpg",
+  "discription": "Last updated 3 mins ago"
+
+}
+]
+
   ngOnInit(): void {
+
   }
+
+
 
 }
